e8018870ccc90e38dc672f6f96f742c0743d177d
Donald Maness <ddmaness@gmail.com>
2018-11-03 19:59:16 -0400

    Implement cors workaround for Yelp API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 import Map from './Map'
 
 class App extends Component {
-  render() {
+	constructor(props) {
+		super(props);
+		this.state = {
 
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972', {
+			headers: {
+				Authorization: 'Bearer wbY30-AwuCYL_fcal_TlDsx3725sox4hMSMH_YwS8pFGpGTHs1g7dqD3tDTbVuMSkrXoupbKjRktLpgR_VvePtm1SQW-5x7zRIsvf4OD1zNWqLfEpEIKYP-LBx3eW3Yx'
+			}
+		}).then(response => response.json()).then(response => console.log(JSON.stringify(response))).catch(err => console.error(err))
+	}
+
+  render() {
     return (
       <div className="App">
         <Map/>
